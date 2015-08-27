0ea41ce5dd8b92b93d0d5aebb0e622e5d279e04c
Uladzimir Abramchuk <abramchuk@targetprocess.com>
2015-08-27 12:07:58 +0300

    extract hubs

--- signalr/_connection.py
+++ signalr/_connection.py
@@ -2,7 +2,7 @@ import json
 
 import gevent
 import requests
-
+from signalr.hubs import Hub
 from signalr.transports import get_url, get_transport
 
 
@@ -40,46 +40,3 @@ class Connection:
         if name not in self.__hubs:
             self.__hubs[name] = Hub(name, self)
         return self.__hubs[name]
-
-
-class Hub:
-    def __init__(self, name, connection):
-        self.server = HubServer(name, connection)
-        self.client = HubClient(name, connection)
-
-
-class HubServer:
-    def __init__(self, name, connection):
-        self.name = name
-        self.__connection = connection
-
-    def invoke(self, method, data):
-        self.__connection.send({
-            'H': self.name,
-            'M': method,
-            'A': [data],
-            'I': ++self.__connection.hub_send_counter
-        })
-        self.__connection.hub_send_counter += 1
-
-    def __getattr__(self, method):
-        def _missing(data):
-            self.invoke(method, data)
-
-        return _missing
-
-
-class HubClient(object):
-    def __init__(self, name, connection):
-        self.name = name
-        self.__connection = connection
-
-        def handle(data):
-            inner_data = data['M'][0] if 'M' in data and len(data['M']) > 0 else {}
-            hub = inner_data['H'] if 'H' in inner_data else ''
-            if hub == self.name:
-                method = inner_data['M']
-                arguments = inner_data['A']
-                getattr(self, method)(arguments)
-
-        self.__connection.subscribe(handle)
---
+++ signalr/hubs/_hub.py
@@ -0,0 +1,41 @@
+class Hub:
+    def __init__(self, name, connection):
+        self.server = HubServer(name, connection)
+        self.client = HubClient(name, connection)
+
+
+class HubServer:
+    def __init__(self, name, connection):
+        self.name = name
+        self.__connection = connection
+
+    def invoke(self, method, data):
+        self.__connection.send({
+            'H': self.name,
+            'M': method,
+            'A': [data],
+            'I': ++self.__connection.hub_send_counter
+        })
+        self.__connection.hub_send_counter += 1
+
+    def __getattr__(self, method):
+        def _missing(data):
+            self.invoke(method, data)
+
+        return _missing
+
+
+class HubClient(object):
+    def __init__(self, name, connection):
+        self.name = name
+        self.__connection = connection
+
+        def handle(data):
+            inner_data = data['M'][0] if 'M' in data and len(data['M']) > 0 else {}
+            hub = inner_data['H'] if 'H' in inner_data else ''
+            if hub == self.name:
+                method = inner_data['M']
+                arguments = inner_data['A']
+                getattr(self, method)(arguments)
+
+        self.__connection.subscribe(handle)
